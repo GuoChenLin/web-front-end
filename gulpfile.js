1266ebe916169f9140f314d60ae05bd9dfb0f04d
gzdaijie <gzdaijie@163.com>
2018-05-04 01:55:07 +0800

    Update 2018-05-03T17:55:01.538Z

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 var deploy = require("gulp-gh-pages");
-var options = { 
-    remoteUrl: "https://github.com/gzdaijie/web-front-end.git",
-    branch: "gh-pages"};
+var gulp = require("gulp");
+
+
 gulp.task('deploy', function () {
-    gulp.src("dist/**/*.*")
-        .pipe(deploy(options));
+    return gulp.src("./_book/**/*")
+        .pipe(deploy());
 });
